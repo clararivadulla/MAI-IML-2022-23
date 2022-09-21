790b628b808cbd9d341bebf5f189a2fd795bbf00
Clara Rivadulla Duró <clararivadulladuro@gmail.com>
2022-09-21 12:05:38 +0200

    read_arff_files function

---
+++ main.py
@@ -0,0 +1,3 @@
+import read_arff_files
+if __name__=='__main__':
+    read_arff_files.main()
---
+++ read_arff_files.py
@@ -0,0 +1,19 @@
+from scipy.io import arff
+import pandas as pd
+
+def read_arff_file(filename):
+    arff_file = arff.loadarff(filename) # Load the arff file with the 'loadarff' function
+    df = pd.DataFrame(arff_file[0]) # Create a dataframe 'df' containing the data of the arff file read
+    # Delete the column containing the class of the data set, since it is not necessary for this exercise
+    if 'class' in df.columns:
+        df = df.drop('class', axis=1)
+    if 'Class' in df.columns:
+        df = df.drop('Class', axis=1)
+    return df
+
+def main():
+    # Choose 3 files to read their data and keep every data set inside a dataframe
+    df1 = read_arff_file('datasets/adult.arff')
+    df2 = read_arff_file('datasets/hepatitis.arff')
+    df3 = read_arff_file('datasets/pima_diabetes.arff')
+    return df1, df2, df3
