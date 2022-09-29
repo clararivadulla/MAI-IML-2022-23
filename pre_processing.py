ad93dbb263f6a5d0298ed1471248b7ae82292a05
shawn-azimov <shawn.azimov@estudiantat.upc.edu>
2022-09-29 17:12:59 +0200

    added preprocessing functions

--- pre_processing.py
+++ pre_processing.py
@@ -1,4 +1,6 @@
 import read_arff_files
+from sklearn import preprocessing
+import pandas as pd
 
 def pre_processing(dataframes):
     for df in dataframes:
@@ -17,6 +19,54 @@ def different_types(dataframe):
 def missing_values(dataframe):
     ''' TO DO '''
     return dataframe
+
+def byte_strings_to_strings(df):
+    s_df = df.select_dtypes([object])
+    s_df = s_df.stack().str.decode('utf-8').unstack()
+    for column in s_df:
+        df[column] = s_df[column]
+
+def min_max_scale_columns(df, cols):
+    min_max_scaler = preprocessing.MinMaxScaler()
+    df[cols] = min_max_scaler.fit_transform(df[cols])
+
+def label_encode_columns(df, cols):
+    label_encoder = preprocessing.LabelEncoder()
+    for col in cols:
+        df[col] = label_encoder.fit_transform(df[col])
+
+def one_hot_encode_columns(df, cols):
+    return pd.get_dummies(data=df, columns=cols)
+
+def get_columns_by_type(meta):
+    nominal_cols = []
+    numeric_cols = []
+    for i in range(0, len(meta.names())):
+        if(meta.names()[i].lower() == 'class'):
+            continue
+
+        if (meta.types()[i] == 'nominal'):
+            nominal_cols.append(meta.names()[i])
+
+        if (meta.types()[i] == 'numeric'):
+            numeric_cols.append(meta.names()[i])
+
+    return (nominal_cols, numeric_cols)
+
+def substitute_missing_values_by_mean(df, cols):
+  for col in cols:
+    df[col] = df[col].fillna(df[col].mean())
+
+def substitute_missing_values_by_median(df, cols):
+  for col in cols:
+    df[col] = df[col].fillna(df[col].median())
+
+def substitute_missing_values_by_most_common(df, cols):
+  for col in cols:
+    df[col] = df[col].fillna(df[col].mode().iloc[0])
+
+def drop_rows_with_missing_values(df):
+    df.dropna(axis = 0, inplace=True)
 
 def main():
     dataframes = read_arff_files.main()
