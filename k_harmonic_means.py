587a6195406713e846f56d66ed4359b2d86a2a70
Clara Rivadulla Duró <clararivadulladuro@gmail.com>
2022-10-03 00:02:49 +0200

    Update k_harmonic_means.py

--- k_harmonic_means.py
+++ k_harmonic_means.py
@@ -5,8 +5,10 @@ epsilon = np.finfo(np.float32).eps
 
 # Performance function
 def performance(X, C):
+    
     p = 3
     sum1 = 0
+
     for i in range(len(X)):
         sum2 = 0
         for j in range(len(C)):
@@ -16,32 +18,63 @@ def performance(X, C):
                 distance = np.power(np.linalg(X[i], X[j]), p)
             sum2 += (1 / distance)
         sum1 += (len(C) / sum2)
+
     return sum1
 
 # Membership function
 def membership(C, cj, xi):
+
     distance = np.power(np.linalg(xi - cj), -p-2)
     sum = 0
+
     for j in range(len(C)):
         sum += np.power(np.linalg(xi - C[j]), -p-2)
+
     return distance / sum
 
 # Weight function
 def weight(C, xi):
+
     sum1 = 0
     sum2 = 0
+
     for j in range(len(C)):
         sum1 += np.power(np.linalg(xi - C[j]), -p-2)
         sum2 += np.power(np.linalg(xi - C[j]), -p)
+
     return sum1 / np.power(sum2, 2)
 
 def cj(X, C, cj):
+
     sum1 = 0
     sum2 = 0
+
     for i in range(len(X)):
         sum1 += membership(C, cj, X[i]) * weight(C, X[i]) * X[i]
         sum2 += membership(C, cj, X[i]) * weight(C, X[i])
+
     return sum1 / sum2
 
+def guess_centers(X, k):
+
+    centers = []
+    indexes = np.choice(len(X), k, replace=False)
+
+    for i in range(k):
+        centers.append(indexes[i])
+
+    return centers
+
 def khm(data, k=3):
+
     X = data.copy()
+    C = guess_centers(X, k)
+
+    while(True):
+        C_aux = C
+        for j in range(len(C)):
+            C[j] = cj(X, C, C[j])
+        if np.array_equal(C_aux, C):
+            return C
+
+
