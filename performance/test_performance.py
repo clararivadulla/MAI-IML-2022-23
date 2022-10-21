5fd0c29e103ce0c93b28e9eee2a86338f02f27b8
shawn-azimov <114064942+shawn-azimov@users.noreply.github.com>
2022-10-21 16:00:53 +0200

    update test_performance.py

--- performance/test_performance.py
+++ performance/test_performance.py
@@ -2,7 +2,7 @@ from fuzzy_clustering.fuzzy_c_means import FuzzyCMeans
 from k_means.bisecting_k_means import BisectingKMeans
 from k_means.k_harmonic_means import KHarmonicMeans
 from k_means.k_means import KMeans
-from pre_processing import read_arff_files, iris_pre_processing, cmc_pre_processing, pima_diabetes_pre_processing
+from pre_processing import read_arff_files, iris_pre_processing, cmc_pre_processing, pima_diabetes_pre_processing#, vehicle_pre_processing, segment_pre_processing, vowel_pre_processing
 from metrics.metrics import calculate_metrics
 import matplotlib.pyplot as plt
 
@@ -17,6 +17,15 @@ def test_performance(dataset_name):
     elif dataset_name == 'pima_diabetes':
         df, meta = read_arff_files.read_arff_file('./../datasets/pima_diabetes.arff')
         data, labels = pima_diabetes_pre_processing.main(df)
+#    elif dataset_name == 'vehicle':
+#        df, meta = read_arff_files.read_arff_file('./../datasets/vehicle.arff')
+#        data, labels = vehicle_pre_processing.main(df)
+#    elif dataset_name == 'segment':
+#        df, meta = read_arff_files.read_arff_file('./../datasets/segment.arff')
+#        data, labels = segment_pre_processing.main(df)
+#    elif dataset_name == 'vowel':
+#        df, meta = read_arff_files.read_arff_file('./../datasets/vowel.arff')
+#        data, labels = vowel_pre_processing.main(df, meta)
     else:
         raise NameError(f'Wrong dataset name: {dataset_name}')
 
@@ -34,21 +43,25 @@ def test_performance(dataset_name):
     fig, axes = plt.subplots(2, 2, figsize=(10, 7))
     ax = axes.ravel()
 
+    #https://scikit-learn.org/stable/modules/generated/sklearn.metrics.silhouette_score.html
     ax[0].plot(k_values, [metric[0] for metric in all_metrics])
-    ax[0].set(xticks=k_values, title='Silhouette Scores', xlabel='k', ylabel='score')
+    ax[0].set(xticks=k_values, title='Silhouette Scores', xlabel='k', ylabel='score [-1, 1] (higher = better)')
 
+    #https://scikit-learn.org/stable/modules/clustering.html#davies-bouldin-index
     ax[1].plot(k_values, [metric[1] for metric in all_metrics])
-    ax[1].set(xticks=k_values, title='Davies Bouldin Scores', xlabel='k', ylabel='score')
+    ax[1].set(xticks=k_values, title='Davies Bouldin Scores', xlabel='k', ylabel='score (lower = better, 0 is best)')
 
+    #https://scikit-learn.org/stable/modules/clustering.html#calinski-harabasz-index
     ax[2].plot(k_values, [metric[2] for metric in all_metrics])
-    ax[2].set(xticks=k_values, title='Calinski Harabasz Scores', xlabel='k', ylabel='score')
+    ax[2].set(xticks=k_values, title='Calinski Harabasz Scores', xlabel='k', ylabel='score (higher = better)')
 
+    #https://scikit-learn.org/stable/modules/generated/sklearn.metrics.adjusted_mutual_info_score.html
     ax[3].plot(k_values, [metric[3] for metric in all_metrics])
-    ax[3].set(xticks=k_values, title='Adjusted Mutual Info Scores', xlabel='k', ylabel='score')
+    ax[3].set(xticks=k_values, title='Adjusted Mutual Info Scores (uses actual labels)', xlabel='k', ylabel='score [0, 1] (higher = better)')
 
     plt.tight_layout()
     plt.show()
 
 
 if __name__ == '__main__':
-    test_performance(dataset_name='pima_diabetes')
+    test_performance(dataset_name='iris')
