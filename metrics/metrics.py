cc9341781899008b0c47ea56d4c33b75db000f4a
shawn-azimov <114064942+shawn-azimov@users.noreply.github.com>
2022-10-22 10:16:23 +0200

    test_performance updates

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -2,9 +2,16 @@ from sklearn import metrics
 
 def calculate_metrics(data, predicted_labels, actual_labels, algorithm_name=None, verbose=False):
 
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.silhouette_score.html
     silhouette_score = metrics.silhouette_score(data, predicted_labels)
+
+    # https://scikit-learn.org/stable/modules/clustering.html#davies-bouldin-index
     davies_bouldin_score = metrics.davies_bouldin_score(data, predicted_labels)
+
+    # https://scikit-learn.org/stable/modules/clustering.html#calinski-harabasz-index
     calinski_harabasz_score = metrics.calinski_harabasz_score(data, predicted_labels)
+
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.adjusted_mutual_info_score.html
     adjusted_mutual_info_score = metrics.adjusted_mutual_info_score(actual_labels, predicted_labels)
 
     if verbose:
--- performance/test_performance.py
+++ performance/test_performance.py
@@ -2,9 +2,12 @@ from fuzzy_clustering.fuzzy_c_means import FuzzyCMeans
 from k_means.bisecting_k_means import BisectingKMeans
 from k_means.k_harmonic_means import KHarmonicMeans
 from k_means.k_means import KMeans
-from pre_processing import read_arff_files, iris_pre_processing, cmc_pre_processing, pima_diabetes_pre_processing#, vehicle_pre_processing, segment_pre_processing, vowel_pre_processing
+from pre_processing import read_arff_files, iris_pre_processing, cmc_pre_processing, \
+    pima_diabetes_pre_processing  # , vehicle_pre_processing, segment_pre_processing, vowel_pre_processing
 from metrics.metrics import calculate_metrics
+
 import matplotlib.pyplot as plt
+from sklearn.cluster import AgglomerativeClustering
 
 
 def test_performance(dataset_name):
@@ -17,19 +20,22 @@ def test_performance(dataset_name):
     elif dataset_name == 'pima_diabetes':
         df, meta = read_arff_files.read_arff_file('./../datasets/pima_diabetes.arff')
         data, labels = pima_diabetes_pre_processing.main(df)
-#    elif dataset_name == 'vehicle':
-#        df, meta = read_arff_files.read_arff_file('./../datasets/vehicle.arff')
-#        data, labels = vehicle_pre_processing.main(df)
-#    elif dataset_name == 'segment':
-#        df, meta = read_arff_files.read_arff_file('./../datasets/segment.arff')
-#        data, labels = segment_pre_processing.main(df)
-#    elif dataset_name == 'vowel':
-#        df, meta = read_arff_files.read_arff_file('./../datasets/vowel.arff')
-#        data, labels = vowel_pre_processing.main(df, meta)
+    #    elif dataset_name == 'vehicle':
+    #        df, meta = read_arff_files.read_arff_file('./../datasets/vehicle.arff')
+    #        data, labels = vehicle_pre_processing.main(df)
+    #    elif dataset_name == 'segment':
+    #        df, meta = read_arff_files.read_arff_file('./../datasets/segment.arff')
+    #        data, labels = segment_pre_processing.main(df)
+    #    elif dataset_name == 'vowel':
+    #        df, meta = read_arff_files.read_arff_file('./../datasets/vowel.arff')
+    #        data, labels = vowel_pre_processing.main(df, meta)
     else:
         raise NameError(f'Wrong dataset name: {dataset_name}')
 
-    all_metrics = []
+    all_metrics = {
+        'BisectingKMeans': [],
+        'AgglomerativeClustering': []
+    }
 
     k_values = [2, 3, 4, 5, 6, 7, 8, 9, 10]
     for k in k_values:
@@ -37,27 +43,36 @@ def test_performance(dataset_name):
         bisecting_k_means_metrics = calculate_metrics(data=data,
                                                       predicted_labels=bisecting_k_means_labels,
                                                       actual_labels=labels)
+        all_metrics['BisectingKMeans'].append(bisecting_k_means_metrics)
 
-        all_metrics.append(bisecting_k_means_metrics)
+        agglomerative_clustering = AgglomerativeClustering(n_clusters=k).fit(data)
+        agglomerative_clustering_labels = agglomerative_clustering.labels_
+        agglomerative_clustering_metrics = calculate_metrics(data=data,
+                                                             predicted_labels=agglomerative_clustering_labels,
+                                                             actual_labels=labels)
+        all_metrics['AgglomerativeClustering'].append(agglomerative_clustering_metrics)
 
     fig, axes = plt.subplots(2, 2, figsize=(10, 7))
     ax = axes.ravel()
 
-    #https://scikit-learn.org/stable/modules/generated/sklearn.metrics.silhouette_score.html
-    ax[0].plot(k_values, [metric[0] for metric in all_metrics])
-    ax[0].set(xticks=k_values, title='Silhouette Scores', xlabel='k', ylabel='score [-1, 1] (higher = better)')
+    for key in all_metrics.keys():
+        ax[0].plot(k_values, [metric[0] for metric in all_metrics[key]], label=key)
+        ax[0].set(xticks=k_values, title='Silhouette Scores', xlabel='k', ylabel='score [-1, 1] (higher = better)')
+        ax[0].legend()
 
-    #https://scikit-learn.org/stable/modules/clustering.html#davies-bouldin-index
-    ax[1].plot(k_values, [metric[1] for metric in all_metrics])
-    ax[1].set(xticks=k_values, title='Davies Bouldin Scores', xlabel='k', ylabel='score (lower = better, 0 is best)')
+        ax[1].plot(k_values, [metric[1] for metric in all_metrics[key]], label=key)
+        ax[1].set(xticks=k_values, title='Davies Bouldin Scores', xlabel='k',
+                  ylabel='score (lower = better, 0 is best)')
+        ax[1].legend()
 
-    #https://scikit-learn.org/stable/modules/clustering.html#calinski-harabasz-index
-    ax[2].plot(k_values, [metric[2] for metric in all_metrics])
-    ax[2].set(xticks=k_values, title='Calinski Harabasz Scores', xlabel='k', ylabel='score (higher = better)')
+        ax[2].plot(k_values, [metric[2] for metric in all_metrics[key]], label=key)
+        ax[2].set(xticks=k_values, title='Calinski Harabasz Scores', xlabel='k', ylabel='score (higher = better)')
+        ax[2].legend()
 
-    #https://scikit-learn.org/stable/modules/generated/sklearn.metrics.adjusted_mutual_info_score.html
-    ax[3].plot(k_values, [metric[3] for metric in all_metrics])
-    ax[3].set(xticks=k_values, title='Adjusted Mutual Info Scores (uses actual labels)', xlabel='k', ylabel='score [0, 1] (higher = better)')
+        ax[3].plot(k_values, [metric[3] for metric in all_metrics[key]], label=key)
+        ax[3].set(xticks=k_values, title='Adjusted Mutual Info Scores (uses actual labels)', xlabel='k',
+                  ylabel='score [0, 1] (higher = better)')
+        ax[3].legend()
 
     plt.tight_layout()
     plt.show()
