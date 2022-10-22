aee6ccb889091265c85edcf6b700dc713f23622d
Jana Dabovic <dabovic.jana@bcg.com>
2022-10-22 14:54:48 +0200

    Update metrics.py
    added confusion matrix

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,5 +1,26 @@
 from sklearn import metrics
 
+def confusion_matrix(y_true, y_pred, class_true=None):
+    if y_true.shape != y_pred.shape:
+        raise Exception ("Shapes do not match")
+    cat_true = list(set(y_true))
+    cat_pred = list(set(y_pred))
+    if len(cat_true) != len(cat_pred):
+        raise Exception ("Number of categories are different")
+    n_cat = len(cat_true)
+    n = len(y_true)
+    conf_matrix = pd.DataFrame(np.zeros((n_cat,n_cat), dtype=int))
+    df = pd.DataFrame({'true':y_true, 'predicted': y_pred})
+    if class_true!=None:
+        if len(class_true)!=n_cat:
+            raise Exception ("Class labels do not match number of categories")
+        else:
+            conf.matrix.index=class_true
+    for i in range(n_cat):
+        for j in range(n_cat):
+            conf_matrix.iloc[i,j] = df[df['true']==cat_true[i]][df['predicted']==cat_pred[j]].shape[0]
+    return conf_matrix
+
 def calculate_metrics(data, predicted_labels, actual_labels, algorithm_name=None, verbose=False):
 
     # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.silhouette_score.html
