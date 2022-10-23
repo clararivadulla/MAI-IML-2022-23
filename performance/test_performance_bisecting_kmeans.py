70012edf63d1728281a84739170af5201832c37a
shawn-azimov <114064942+shawn-azimov@users.noreply.github.com>
2022-10-23 22:43:45 +0200

    updates for bisecting kmeans

--- performance/test_performance_bisecting_kmeans.py
+++ performance/test_performance_bisecting_kmeans.py
@@ -1,16 +1,10 @@
 import sys
 sys.path.append('../')
-from fuzzy_clustering.fuzzy_c_means import FuzzyCMeans
 from k_means.bisecting_k_means import BisectingKMeans
-from k_means.k_harmonic_means import KHarmonicMeans
 from k_means.k_means import KMeans
 from pre_processing import read_arff_files, iris_pre_processing, cmc_pre_processing, pima_diabetes_pre_processing
 from validation_metrics.metrics import calculate_metrics
-from figures.plots import plot_metrics, plot_clusters
-
-
-from sklearn.cluster import AgglomerativeClustering
-
+from figures.plots import plot_metrics
 
 def test_performance(dataset_name):
     if dataset_name == 'iris':
@@ -26,17 +20,13 @@ def test_performance(dataset_name):
         raise NameError(f'Wrong dataset name: {dataset_name}')
 
     all_metrics = {
-#        'KMeans': [],
         'BisectingKMeans': []
-#        'AgglomerativeClustering': [],
-#        'FuzzyCMeans': [],
-#        'KHarmonicMeans': []
     }
 
     num_trials_values = [1, 5, 10, 15, 20, 25]
     num_k_values = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    for n in num_k_values:
+    for n in num_trials_values:
 
         print(f'Running algorithms with num_k_values={n}')
         # k_means = KMeans(k=n, n_repeat=1)
@@ -54,7 +44,7 @@ def test_performance(dataset_name):
                                                       actual_labels=labels)
         all_metrics['BisectingKMeans'].append(bisecting_k_means_metrics)
 
-    plot_metrics(metrics=all_metrics, k_values=num_k_values, dataset_name=dataset_name, x_label='k')
+    plot_metrics(metrics=all_metrics, k_values=num_trials_values, dataset_name=dataset_name, x_label='k')
 
 if __name__ == '__main__':
-    test_performance(dataset_name='cmc')
+    test_performance(dataset_name='iris')
