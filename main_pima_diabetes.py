b5bef3bd6ed9c59e1071d9f181d718775c03f0ea
Clara Rivadulla Duró <clararivadulladuro@gmail.com>
2022-10-23 22:53:33 +0200

    Update main_pima_diabetes.py

--- main_pima_diabetes.py
+++ main_pima_diabetes.py
@@ -29,14 +29,14 @@ def main():
                                                          algorithm_name='Agglomerative Clustering',
                                                          verbose=True)
     scores.append(agglomerative_clustering_metrics)
-    scatter_plot(agglomerative_clustering_labels, data, (0, 1), title='Pima Diabetes dataset\nAgglomerative Clustering with 3 clusters')
+    scatter_plot(agglomerative_clustering_labels, data, (2, 5), title='Pima Diabetes dataset\nAgglomerative Clustering with 3 clusters')
 
     # Mean Shift clustering
     print(
         '\n**************************************************\nMean Shift\n**************************************************')
 
     bandwidth_value = estimate_bandwidth(data, quantile=0.75)
-    meanshift_clustering = MeanShift(bandwidth=bandwidth_value, seed_dim=None, binSeed=False, allCluster=True).fit(data)
+    meanshift_clustering = MeanShift(bandwidth=bandwidth_value, seeds=None, bin_seeding=True, cluster_all=True).fit(data)
     mean_shift_clustering_labels = meanshift_clustering.labels_
     mean_shift_clustering_metrics = calculate_metrics(data=data,
                                                       predicted_labels=mean_shift_clustering_labels,
@@ -44,7 +44,7 @@ def main():
                                                       algorithm_name='Mean Shift Clustering',
                                                       verbose=True)
     scores.append(mean_shift_clustering_metrics)
-    scatter_plot(mean_shift_clustering_labels, data, (0, 1), title='Pima Diabetes dataset\nMean Shift Clustering')
+    scatter_plot(mean_shift_clustering_labels, data, (2, 5), title='Pima Diabetes dataset\nMean Shift Clustering')
 
     # K-Means
     print(
@@ -58,7 +58,7 @@ def main():
                                         algorithm_name='K-Means',
                                         verbose=True)
     scores.append(k_means_metrics)
-    scatter_plot(k_means_labels, data, (0, 1), title='Pima Diabetes dataset\nK-Means with 3 clusters')
+    scatter_plot(k_means_labels, data, (2, 5), title='Pima Diabetes dataset\nK-Means with 3 clusters')
 
     # Bisecting K Means
     print(
@@ -70,7 +70,7 @@ def main():
                                                   algorithm_name='Bisecting K-Means',
                                                   verbose=True)
     scores.append(bisecting_k_means_metrics)
-    scatter_plot(bisecting_k_means_labels, data, (0, 1), title='Pima Diabetes dataset\nBisecting K-Means with 3 clusters')
+    scatter_plot(bisecting_k_means_labels, data, (2, 5), title='Pima Diabetes dataset\nBisecting K-Means with 3 clusters')
 
     # K-Harmonic Means
     print(
@@ -84,7 +84,7 @@ def main():
                                                  algorithm_name='K-Harmonic Means',
                                                  verbose=True)
     scores.append(k_harmonic_means_metrics)
-    scatter_plot(k_harmonic_means_labels, data, (0, 1), title='Pima Diabetes dataset\nK-Harmonic Means with 3 clusters')
+    scatter_plot(k_harmonic_means_labels, data, (2, 5), title='Pima Diabetes dataset\nK-Harmonic Means with 3 clusters')
 
     # Fuzzy C-Means
     print(
@@ -98,7 +98,7 @@ def main():
                                               algorithm_name='Fuzzy C-Means',
                                               verbose=True)
     scores.append(fuzzy_c_means_metrics)
-    scatter_plot(fuzzy_c_means_labels, data, (0, 1), title='Pima Diabetes dataset\nFuzzy C-Means with 3 clusters')
+    scatter_plot(fuzzy_c_means_labels, data, (2, 5), title='Pima Diabetes dataset\nFuzzy C-Means with 3 clusters') # glucose and insuline
 
     # Save the scores in a dataframe for future graphs
     scores_df = pd.DataFrame(scores, columns=['Algorithm', 'Silhouette Score', 'Davies Bouldin Score',
