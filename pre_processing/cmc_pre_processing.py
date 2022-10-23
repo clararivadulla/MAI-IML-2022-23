61040917196d625968a2e083f20061ef503a7f26
Jana Dabovic <dabovic.jana@bcg.com>
2022-10-23 09:56:28 +0200

    Update cmc_pre_processing.py

--- pre_processing/cmc_pre_processing.py
+++ pre_processing/cmc_pre_processing.py
@@ -1,13 +1,18 @@
 from pre_processing import pre_processing_functions
 
 
-def main(df, numerical_only=False):
+def main(df, numerical_only=False, norm_type="gaussian"):
     class_labels = pre_processing_functions.remove_and_return_class_column(df).to_numpy()
 
     pre_processing_functions.byte_strings_to_strings(df)
-    df = pre_processing_functions.one_hot_encode_columns(df,
-                                                         ['weducation', 'heducation', 'hoccupation', 'living_index'])
-    pre_processing_functions.standardize_columns(df, ['wage', 'children'])
+    df = pre_processing_functions.one_hot_encode_columns(df, ['weducation', 'heducation', 'hoccupation', 'living_index'])
+    print(df)
+    If norm_type=='gaussian':
+        pre_processing_functions.standardize_columns(df, ['wage', 'children'])
+    elif norm_type == "l2":
+        pre_processing_functions.normalize_columns(df, ['wage', 'children'])
+    elif normalization=="min_max":
+        pre_processing_functions.min_max_scale_columns(df, ['wage', 'children'])
 
     if numerical_only:
         df = df[['wage', 'children']]
