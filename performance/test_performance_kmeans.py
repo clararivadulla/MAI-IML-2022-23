12bf7d76a5465e9e6a3c76ba27efe5aea2882822
Jana Dabovic <dabovic.jana@bcg.com>
2022-10-23 02:38:15 +0200

    Create test_performance_kmeans.py

---
+++ performance/test_performance_kmeans.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.append('../')
+from k_means.k_means import KMeans
+from pre_processing import read_arff_files, iris_pre_processing, cmc_pre_processing, pima_diabetes_pre_processing
+from metrics.metrics import calculate_metrics
+from figures.plots import plot_metrics
+
+
+from sklearn.cluster import AgglomerativeClustering
+
+def test_performance(dataset_name):
+    if dataset_name == 'iris':
+        df, meta = read_arff_files.read_arff_file('./../datasets/iris.arff')
+        data, labels = iris_pre_processing.main(df)
+    elif dataset_name == 'pima_diabetes':
+        df, meta = read_arff_files.read_arff_file('./../datasets/pima_diabetes.arff')
+        data, labels = pima_diabetes_pre_processing.main(df)
+    elif dataset_name == 'cmc':
+        df, meta = read_arff_files.read_arff_file('./../datasets/cmc.arff')
+        data, labels = cmc_pre_processing.main(df)
+    else:
+        raise NameError(f'Wrong dataset name: {dataset_name}')
+
+    all_metrics = {'KMeans': []}
+    k_values = [2, 3, 4, 5, 6, 7]
+    for k in k_values:
+        print(f'Running algorithms with k={k}')
+        k_means = KMeans(k=k)
+        k_means.train(data)
+        k_means_labels, _ = k_means.classify(data)
+
+        k_means_metrics = calculate_metrics(data=data,
+                                            predicted_labels=k_means_labels,
+                                            actual_labels=labels,
+                                            verbose = True)
+        all_metrics['KMeans'].append(k_means_metrics)
+
+    plot_metrics(metrics=all_metrics, k_values=k_values, dataset_name=dataset_name)
+
+if __name__ == '__main__':
+    test_performance(dataset_name='iris')
