6dfefa056ed8eb6b5a93d53f3cad24c32885f3c1
Jana Dabovic <dabovic.jana@bcg.com>
2022-10-23 09:56:35 +0200

    Update pima_diabetes_pre_processing.py

--- pre_processing/pima_diabetes_pre_processing.py
+++ pre_processing/pima_diabetes_pre_processing.py
@@ -3,7 +3,7 @@ from pre_processing import pre_processing_functions
 
 def main(df):
     class_labels = pre_processing_functions.remove_and_return_class_column(df).to_numpy()
-
-    pre_processing_functions.standardize_columns(df, df.columns)
+    pre_processing_functions.normalize_columns(df, df.columns)
+    #pre_processing_functions.standardize_columns(df, df.columns)
     data = pre_processing_functions.df_to_numeric_array(df)
     return data, class_labels
