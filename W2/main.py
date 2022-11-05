e26acbe72fd70941d1f80517240449b409d295ce
shawn-azimov <114064942+shawn-azimov@users.noreply.github.com>
2022-11-05 14:50:19 +0100

    refactor main

--- W2/main.py
+++ W2/main.py
@@ -1,8 +1,14 @@
 import main_cmc
 import main_iris
 import main_pima_diabetes
+from pre_processing import read_arff_files, iris_pre_processing
+import run_clustering
 
 if __name__ == '__main__':
-    main_cmc.main()
-    main_iris.main()
-    main_pima_diabetes.main()
+    df, meta = read_arff_files.main('iris.arff')
+    data, labels = iris_pre_processing.main(df)
+    run_clustering.run(data, labels, dataset_name="Iris")
+
+    #main_cmc.main()
+    #main_iris.main()
+    #main_pima_diabetes.main()
