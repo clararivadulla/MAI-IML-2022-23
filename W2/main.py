906a9be270c2f347761dc5d3547de5819fd5f261
shawn-azimov <114064942+shawn-azimov@users.noreply.github.com>
2022-11-06 17:31:47 +0100

    switch to run_clustering for all 3 datasets

--- W2/main.py
+++ W2/main.py
@@ -1,13 +1,18 @@
-import main_cmc
-import main_iris
-import main_pima_diabetes
-from pre_processing import read_arff_files, iris_pre_processing
+from pre_processing import read_arff_files, iris_pre_processing, cmc_pre_processing, pima_diabetes_pre_processing
 import run_clustering
 
 if __name__ == '__main__':
     df, meta = read_arff_files.main('iris.arff')
     data, labels = iris_pre_processing.main(df)
     run_clustering.run(data, labels, dataset_name="Iris")
+
+    df, meta = read_arff_files.main('cmc.arff')
+    data, labels = cmc_pre_processing.main(df)
+    run_clustering.run(data, labels, dataset_name="CMC")
+
+    df, meta = read_arff_files.main('pima_diabetes.arff')
+    data, labels = pima_diabetes_pre_processing.main(df)
+    run_clustering.run(data, labels, dataset_name="Pima Diabetes")
 
     #main_cmc.main()
     #main_iris.main()
