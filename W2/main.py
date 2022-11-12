3a28751dbebb5e9a6da7189322dd2c4aece683c2
Jana Dabovic <dabovic.jana@bcg.com>
2022-11-12 23:55:58 +0100

    Update main.py

--- W2/main.py
+++ W2/main.py
@@ -4,12 +4,12 @@ import run_clustering
 if __name__ == '__main__':
     df, meta = read_arff_files.main('vowel.arff')
     data, labels = vowel_pre_processing.main(df, meta, norm_type='min_max')
-    run_clustering.run(data, labels, dataset_name="Vowel", k=11, num_features=[0.8, 0.9, 0.95], plot_3D=False)
+    run_clustering.run(data, labels, dataset_name="Vowel", k=11, num_features=[12], plot_3D=False)
 
     df, meta = read_arff_files.main('iris.arff')
     data, labels = iris_pre_processing.main(df)
-    run_clustering.run(data, labels, dataset_name="Iris", k=3, num_features=[2,3], plot_3D=False)
+    run_clustering.run(data, labels, dataset_name="Iris", k=3, num_features=[2], plot_3D=False)
 
     df, meta = read_arff_files.main('pima_diabetes.arff')
     data, labels = pima_diabetes_pre_processing.main(df)
-    run_clustering.run(data, labels, dataset_name="Pima Diabetes", k=2, num_features=[0.8, 0.9, 0.95], plot_3D=False)
+    run_clustering.run(data, labels, dataset_name="Pima Diabetes", k=4, num_features=[5], plot_3D=False)
