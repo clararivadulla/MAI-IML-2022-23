30e868b9654e33fd96012b9e9dc0b1c5947b952c
Jana Dabovic <dabovic.jana@bcg.com>
2022-11-26 15:04:39 +0100

    Update distance_metrics.py

--- Work 3/metrics/distance_metrics.py
+++ Work 3/metrics/distance_metrics.py
@@ -3,16 +3,8 @@ import pandas as pd
 
 # how do we deal with categorical values?
 
-def euclidean(a, b):
-    return np.linalg.norm(a - b)
-
-def euclidean_v2(a, b, axis=1):
-    return np.linalg.norm(a - b, axis=axis)
-
-
 def cosine(a, b):
     return np.dot(a, b) / (np.linalg.norm(a, axis=1) * np.linalg.norm(b))
 
-
-def minkowski(a, b, r):
+def minkowski(a, b, r=2):
     return np.sum(np.abs(a - b) ** r, axis=1) ** (1 / r)
