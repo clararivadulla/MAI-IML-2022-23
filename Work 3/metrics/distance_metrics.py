85865d443bc6303a3370e2d2c900edd6501b3afb
Jana Dabovic <dabovic.jana@bcg.com>
2022-12-09 18:05:25 +0100

    Update distance_metrics.py

--- Work 3/metrics/distance_metrics.py
+++ Work 3/metrics/distance_metrics.py
@@ -1,21 +1,41 @@
 import numpy as np
 
-# how do we deal with categorical values? Is there a need to deal with it?
+def cosine(a, b, nom_cols=None, num_cols=None):
+    if nom_cols != None:
+        # Calculate share of nominal variables where 2 entries are the same, on scale from 0 to 1
+        nom_dist = np.sum(a.iloc[:,nom_cols] == b[nom_cols], axis=1) / len(nom_cols)
+        # Convert nominal distance to the scale of the output of cosine function, -1 to 1
+        nom_dist_scaled = nom_dist*2-1
+        num_dist = np.dot(a.iloc[:,num_cols], b[num_cols]) / (np.linalg.norm(a.iloc[:,num_cols], axis=1) * np.linalg.norm(b[num_cols]))
+        return len(nom_cols)/(len(nom_cols)+len(num_cols))*nom_dist_scaled + len(num_cols)/(len(nom_cols)+len(num_cols))*num_dist
+    else:
+        return np.dot(a, b) / (np.linalg.norm(a, axis=1) * np.linalg.norm(b))
 
-def cosine(a, b):
     # Check if there is a zero vector and add 1e-5 to be able to calculate the distance
-    a_zero_vector = np.where(~a.any(axis=1))[0]
+    '''a_zero_vector = np.where(~a.any(axis=1))[0]
     b_zero_vector = np.where(~b.any())[0]
     if len(a_zero_vector)>0:
         a.iloc[a_zero_vector,:] += 1e-5
     if len(b_zero_vector)>0:
-        b += 1e-5
-    return np.dot(a, b) / (np.linalg.norm(a, axis=1) * np.linalg.norm(b))
+        b += 1e-5'''
 
-def minkowski(a, b, r=2):
-    return np.sum(np.abs(a - b) ** r, axis=1) ** (1 / r)
+def minkowski(a, b, r=2, nom_cols=None, num_cols=None):
+    if nom_cols != None:
+        # Calculate share of nominal variables where 2 entries are the same, on scale from 0 to 1
+        nom_dist = np.sum(a.iloc[:,nom_cols] == b[nom_cols], axis=1) / len(nom_cols)
+        num_dist = np.sum(np.abs(a.iloc[:,num_cols] - b[num_cols]) ** r, axis=1) ** (1 / r)
+        return len(nom_cols)/(len(nom_cols)+len(num_cols))*nom_dist + len(num_cols)/(len(nom_cols)+len(num_cols))*num_dist
+    else:
+        return np.sum(np.abs(a - b) ** r, axis=1) ** (1 / r)
 
-def clark(a,b):
-    c=a+b
-    c[c==0] += 1e-5
-    return np.sum((a-b)**2/(c**2), axis=1)
+def clark(a,b, nom_cols=None, num_cols=None):
+    '''c=a+b
+    c[c==0] += 1e-5'''
+    if nom_cols != None:
+        # Calculate share of nominal variables where 2 entries are the same, on scale from 0 to 1
+        nom_dist = np.sum(a.iloc[:,nom_cols] == b[nom_cols], axis=1) / len(nom_cols)
+        c = a.iloc[:,num_cols] + b[num_cols]
+        num_dist = np.sum((a.iloc[:,num_cols] - b[num_cols])**2/(c**2), axis=1)
+        return len(nom_cols)/(len(nom_cols)+len(num_cols))*nom_dist + len(num_cols)/(len(nom_cols)+len(num_cols))*num_dist
+    else:
+        return np.sum((a-b)**2/(c**2), axis=1)
