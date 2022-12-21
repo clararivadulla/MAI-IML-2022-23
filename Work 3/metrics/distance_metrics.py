fixed scaling of nom_dist for cosine

Work 3/metrics/distance_metrics.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+def transform_scale(a):
+    max = 1.
+    new_a = ( a * 2 ) + 1
+    dif = max - new_a
+    if dif % 0.5 == 0:
+        new_a = dif + 1
+    else:
+        new_a = new_a + (dif*2)
+    return new_a
+
 def cosine(a, b, nom_cols=None, num_cols=None):
 
     nom_dist = 0
@@ -9,7 +19,8 @@ def cosine(a, b, nom_cols=None, num_cols=None):
         # Calculate share of nominal variables where 2 entries are the same, on scale from 0 to 1
         nom_dist = np.sum(a.iloc[:,nom_cols] == b[nom_cols], axis=1) / len(nom_cols)
         # Convert nominal distance to the scale of the output of cosine function, -1 to 1
-        nom_dist = nom_dist*2-1
+#        nom_dist = nom_dist*2-1
+        nom_dist = transform_scale(nom_dist)
     return nom_dist + num_dist
 
     # Check if there is a zero vector and add 1e-5 to be able to calculate the distance