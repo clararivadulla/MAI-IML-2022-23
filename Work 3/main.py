c1cff812216f14581dd7550e3a0f07dce3c3f590
Jana Dabovic <dabovic.jana@bcg.com>
2022-12-22 19:33:51 +0100

    Update main.py

--- Work 3/main.py
+++ Work 3/main.py
@@ -1,10 +1,12 @@
 from performance.performance import test_performance
 from pre_processing import data
 
-if __name__ == '__main__':
 
+
+if __name__ == '__main__':
     # In this file, we run kNN for every dataset with the best parameters found for that dataset
-    print(f'\n··················································\nPERFORMANCE TESTS FOR VOWEL DATASET\n··················································')
+    print(
+        f'\n··················································\nPERFORMANCE TESTS FOR VOWEL DATASET\n··················································')
     dataset_name = 'vowel'
     vowel_data, numeric_cols, nominal_cols = data.get_data(dataset_name=dataset_name)
     vowel_results_sorted_by_accuracy = test_performance(data=vowel_data, dataset_name=dataset_name, numeric_cols=numeric_cols, nominal_cols=nominal_cols, verbose=True)
@@ -20,3 +22,5 @@ if __name__ == '__main__':
     satimage_results_sorted_by_accuracy = test_performance(data=sat_image_data, dataset_name=dataset_name, numeric_cols=numeric_cols, nominal_cols=nominal_cols, verbose=True)
 
     print()
+
+    run_tests()
