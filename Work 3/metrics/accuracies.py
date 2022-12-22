86eaaf9e440db0c665123faaeed7aee29a204354
Jana Dabovic <dabovic.jana@bcg.com>
2022-12-22 19:31:07 +0100

    Update accuracies.py

--- Work 3/metrics/accuracies.py
+++ Work 3/metrics/accuracies.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy.stats import ttest_rel
 
 def accuracy(labels, predictions):
     df = {'true labels':labels, 'predictions':predictions}
@@ -7,3 +8,20 @@ def accuracy(labels, predictions):
     incorrect = len(labels) - correct
     share = (correct / len(labels))
     return correct, incorrect, share
+
+def t_tests(acc1, acc2, acc3):
+    matrix = np.ones((len(acc1),len(acc1)))
+    results = []
+    for i in range(len(acc1)):
+        set1 = [acc1[i], acc2[i], acc3[i]]
+        for j in range(i+1,len(acc1)):
+            set2 = [acc1[j], acc2[j], acc3[j]]
+            p=ttest_rel(set1,set2)[1]
+            matrix[i,j] = p
+            if p<0.05:
+                results.append([i,j,set1, set2,p])
+    print(results)
+    return matrix, results
+
+
+
